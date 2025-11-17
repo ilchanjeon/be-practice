0ceef6948a5e01d75c2dd1fef6dc25e80622abcc
chan <winters63@naver.com>
2025-11-17 16:40:19 +0900

    index change -> swagger's index

--- src/main/java/com/test/dockertest/DockerTestApplication.java
+++ src/main/java/com/test/dockertest/DockerTestApplication.java
@@ -1,17 +1,20 @@
 package com.test.dockertest;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @SpringBootApplication
 public class DockerTestApplication {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    @GetMapping("/")
+    public void home(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui/index.html");
     }
 
     public static void main(String[] args) {
